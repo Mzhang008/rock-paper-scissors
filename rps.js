4ae48fcc2f10acc2d14c37b19dc28db6c121d850
Mzhang008 <mzhang008@gmail.com>
2021-08-02 16:09:07 -0500

    Added UI for RPS game

--- rps.js
+++ rps.js
@@ -1,3 +1,40 @@
+let playerScore = 0;
+let computerScore = 0;
+const moves = document.querySelectorAll('.move');
+moves.forEach(move => {
+    move.addEventListener("click", function() {
+        const results = document.querySelector('#results');
+        let scoreDiv = document.createElement('div');
+        let playerChoice = move.getAttribute("playerMove");
+        console.log(playerChoice);
+        let computerChoice = computerPlay();
+        
+        if (rpsRound(playerChoice, computerChoice) === 0) { //win
+            scoreDiv.innerText = ("You Win! " + playerChoice.charAt(0).toUpperCase() +
+                playerChoice.slice(1) + " beats " + computerChoice);
+            playerScore++;
+        } else if (rpsRound(playerChoice, computerChoice) === 1)  { //draw
+            scoreDiv.innerText = ("Draw! " + playerChoice + " draws with " + computerChoice);
+        } else { //lose
+            scoreDiv.innerText = ("You Lose! " + computerChoice + " beats " + playerChoice.charAt(0).toUpperCase() +
+                playerChoice.slice(1));
+            computerScore++;
+        }
+        
+        
+        if (playerScore === 5) {
+            results.textContent = "You win the match! Player score: " + playerScore + ", Computer score: " + computerScore;
+        } else if (computerScore === 5) {
+            results.textContent = "You lose the match... Player score: " + playerScore + ", Computer score: " + computerScore;
+        } else {
+            results.textContent = "Player score: " + playerScore + "\nComputer Score: " + computerScore;
+        };
+        results.appendChild(scoreDiv);
+
+    });
+});
+
+
 function computerPlay() {
     switch (Math.floor(Math.random() * 3)) {
         case 0:
@@ -12,7 +49,7 @@ function computerPlay() {
 }
 
 function rpsRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+    if ((playerSelection === null) || (computerSelection === null)) return;
     if ((playerSelection === "rock" && computerSelection === "Scissors" ) ||
         (playerSelection === "paper" && computerSelection === "Rock") ||
         (playerSelection === "scissors" && computerSelection === "Paper")) {
@@ -24,9 +61,12 @@ function rpsRound(playerSelection, computerSelection) {
     }
 }
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    while (playerScore < 5 && computerScore < 5) {
+    playerScore = 0;
+    computerScore = 0;
+    const results = document.querySelector('#results');
+    results.textContent = "Player score: " + playerScore + "\nComputer Score: " + computerScore;
+    
+    /*while (playerScore < 5 && computerScore < 5) {
         let playerChoice = prompt("Enter Rock, Paper, or Scissors against the computer: ", "Rock/Paper/Scissors");
         computerChoice = computerPlay();
         if (rpsRound(playerChoice, computerChoice) === 0) { //win
@@ -40,10 +80,11 @@ function game() {
                 playerChoice.slice(1));
             computerScore++;
         }
-    }
-    if (playerScore === 5) {
+    }*/
+    
+    /*if (playerScore === 5) {
         console.log("You win the match! Player score: " + playerScore + ", Computer score: " + computerScore);
     } else {
         console.log("You lose the match... Player score: " + playerScore + ", Computer score: " + computerScore);
-    }
+    }*/
 }
